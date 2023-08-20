11a4bf43927b435a55b6babed72df8a5d73b8673
Amrit Adhikari <awmrit@gmail.com>
2023-08-20 13:38:35 +0545

    add play music in loop

--- lib/screens/motivational.dart
+++ lib/screens/motivational.dart
@@ -1,5 +1,6 @@
 import 'package:awesome_sounds/global/const.dart';
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 
 class Motivational extends StatefulWidget {
   const Motivational({super.key});
@@ -9,6 +10,8 @@ class Motivational extends StatefulWidget {
 }
 
 class _MotivationalState extends State<Motivational> {
+  final _player = AudioPlayer();
+  bool isPlaying = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,8 +22,44 @@ class _MotivationalState extends State<Motivational> {
       ),
       body: Column(
         children: [
+          IconButton(
+            onPressed: () async {
+              if (isPlaying) {
+                await _player.pause();
+                setState(() {
+                  isPlaying = false;
+                });
+              } else {
+                await _player.setAsset('assets/sounds/test.mp3');
+
+                // Set loop mode
+                _player.setLoopMode(LoopMode.one);
+
+                await _player.play();
+
+                setState(() {
+                  isPlaying = true;
+                });
+              }
+            },
+            icon: Icon(
+              isPlaying ? Icons.pause : Icons.play_arrow,
+              color: primaryColor,
+            ),
+          ),
           const SizedBox(height: 20),
-          //make a playing and pause button
+          const Center(
+              child: Text(
+            'Play',
+            style: TextStyle(),
+          )),
+          const SizedBox(height: 20),
+          Center(
+            child: Text(
+              "Motivational",
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+          ),
           const SizedBox(height: 20),
           const Center(
               child: Text(
@@ -39,9 +78,7 @@ class _MotivationalState extends State<Motivational> {
               borderRadius: BorderRadius.circular(20),
             ),
           ),
-
           const SizedBox(height: 20),
-
           Center(
             child: Text(
               "Motivational",
