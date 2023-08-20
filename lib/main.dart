6061db085c66fbd6fb2ba084fc1cf9396ed8fc30
Amrit Adhikari <awmrit@gmail.com>
2023-08-20 13:16:45 +0545

    add sound

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:awesome_sounds/global/const.dart';
-import 'package:awesome_sounds/screens/home_page.dart';
 import 'package:awesome_sounds/screens/motivational.dart';
 import 'package:flutter/material.dart';
 
